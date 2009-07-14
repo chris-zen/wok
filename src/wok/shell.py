87092fe8ca44f79cfac2e744cdabe88839d803c9
Christian Pérez-Llamas <chrispz@gmail.com>
2009-07-14 13:55:16 +0000

    small changes in shell

--- src/wok/shell.py
+++ src/wok/shell.py
@@ -7,21 +7,23 @@ class ShellException(Exception):
     def __str__(self):
         return self.command + "\n" + self.error
 
-def run(command, input=None, cwd=None, env=None, check_returncode=True, log=None):
+def run(command, input=None, cwd=None, env=None, check_returncode=True,
+        log=None, log_cmd=True, log_out=True, log_err=True):
+    
     p = subprocess.Popen(args=command, 
                          stdin=subprocess.PIPE, 
                          stdout=subprocess.PIPE, 
                          stderr=subprocess.PIPE,
                          shell=True, cwd=cwd, env=env)
     
-    if log is not None:
+    if log_cmd and log is not None:
         log.info(command)
 
     (output, error) = p.communicate(input)
     
     if log is not None:
-        if output is not None and len(output) > 0: log.info(output)
-        if error is not None and len(error) > 0: log.error(error)
+        if log_out and output is not None and len(output) > 0: log.info(output)
+        if log_err and error is not None and len(error) > 0: log.error(error)
     
     ret = { "command" : command, "returncode" : p.returncode,
             "input" : input, "output" : output, "error" : error }
