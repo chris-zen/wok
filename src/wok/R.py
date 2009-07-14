8879520cfa02f735c980393896ad34dd49a8815e
Christian Pérez-Llamas <chrispz@gmail.com>
2009-07-14 16:25:46 +0000

    small changes in shell

--- src/wok/R.py
+++ src/wok/R.py
@@ -1,9 +1,9 @@
 
-from wok.shell import *
+import wok.shell
 
 def run(file, args = None, cmd = "R", log=None):
     options = "--vanilla --slave -f " + file
     if args is not None:
         options += " --args " + args
 
-    return shell(cmd + " " + options, log=log)
+    return wok.shell.run(cmd + " " + options, log=log)
